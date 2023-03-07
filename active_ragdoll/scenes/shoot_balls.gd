e72921b74d3b8183c5437ac327dae52396274715
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-03-06 23:03:29 -0800

    Arrange to vsk coding style.

---
+++ active_ragdoll/scenes/shoot_balls.gd
@@ -0,0 +1,16 @@
+extends Camera3D
+
+@export var bullet_scene: PackedScene
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("fire"):
+		var mouse_pos = get_viewport().get_mouse_position()
+		var ray_length = 100
+		var from = project_ray_origin(mouse_pos)
+		var to = from + project_ray_normal(mouse_pos) * ray_length
+		var bullet = bullet_scene.instantiate()
+		add_child(bullet)
+		bullet.look_at(to)
+		bullet.top_level = true
+		bullet.fire(10)
