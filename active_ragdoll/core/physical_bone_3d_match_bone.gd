8366dbbb1b22bbeb2949beae4987f211d2ce81ba
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-03-12 11:29:47 -0700

    Working on using any humanoid character.

---
+++ active_ragdoll/core/physical_bone_3d_match_bone.gd
@@ -0,0 +1,13 @@
+extends PhysicalBone3D
+
+@export var target_skeleton: Skeleton3D
+
+
+func _physics_process(delta):
+	var target_transform: Transform3D = target_skeleton.get_bone_global_pose(get_bone_id())
+	var transform: Transform3D = get_parent().get_bone_global_pose(get_bone_id())
+	var difference: Basis = target_transform.basis * transform.basis.inverse()
+
+	var torque: Vector3 = (4000.0 * difference.get_euler()) - (80.0 * angular_velocity)
+
+	angular_velocity += torque * delta
